f7da189ee3eb7444a2a5106af5b889b17491b743
Hanieh Navab <107445808+Hani-navab@users.noreply.github.com>
2022-06-14 12:27:54 +1000

    Add files via upload

---
+++ frmProduct.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GridDemo
+{
+    partial class frmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dbgProduct = new System.Windows.Forms.DataGridView();
+            this.ProductID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDelete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbgProduct
+            // 
+            this.dbgProduct.AllowUserToAddRows = false;
+            this.dbgProduct.AllowUserToDeleteRows = false;
+            this.dbgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbgProduct.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductID,
+            this.ProductName,
+            this.btnDelete});
+            this.dbgProduct.Location = new System.Drawing.Point(12, 12);
+            this.dbgProduct.Name = "dbgProduct";
+            this.dbgProduct.ReadOnly = true;
+            this.dbgProduct.Size = new System.Drawing.Size(378, 150);
+            this.dbgProduct.TabIndex = 0;
+            this.dbgProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dbgProduct_CellContentClick);
+            // 
+            // ProductID
+            // 
+            this.ProductID.DataPropertyName = "ProductID";
+            this.ProductID.HeaderText = "کد";
+            this.ProductID.Name = "ProductID";
+            this.ProductID.ReadOnly = true;
+            this.ProductID.Width = 50;
+            // 
+            // ProductName
+            // 
+            this.ProductName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ProductName.DataPropertyName = "Name";
+            this.ProductName.HeaderText = "نام کالا";
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            // 
+            // btnDelete
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDelete.DefaultCellStyle = dataGridViewCellStyle2;
+            this.btnDelete.HeaderText = "حذف";
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.ReadOnly = true;
+            this.btnDelete.Text = "حذف";
+            this.btnDelete.UseColumnTextForButtonValue = true;
+            // 
+            // frmProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 273);
+            this.Controls.Add(this.dbgProduct);
+            this.Name = "frmProduct";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "کالا";
+            this.Load += new System.EventHandler(this.frmProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dbgProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dbgProduct;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewButtonColumn btnDelete;
+    }
+}
---
+++ frmProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GridDemo
+{
+    public partial class frmProduct : Form
+    {
+        public frmProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProduct_Load(object sender, EventArgs e)
+        {
+            dbgProduct.DataSource = DataAccess.Product.GetAllProducts().Tables[0];
+        }
+
+        private void dbgProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex==0)
+            {
+                if (Business.Product.DeleteProduct(dbgProduct[1, e.RowIndex].Value))
+                    ((DataTable)dbgProduct.DataSource).Rows.Find(dbgProduct[1, e.RowIndex].Value).Delete();
+            }
+        }
+    }
+}
